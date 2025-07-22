37711a30f2a71898262fa96e00356563b8fed65d
Parthkuamr Rathod <parthrathodtk@gmail.com>
2025-07-22 13:16:34 +0530

    Collision in Elements and Food Items

--- Linked-List-Snake/include/Element/ElementService.h
+++ Linked-List-Snake/include/Element/ElementService.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <SFML/System/Vector2.hpp>
+#include "LinkedList/Node.h"
 
 namespace Element
 {
@@ -13,6 +14,7 @@ namespace Element
 		std::vector<Obstacle*> obstacle_list;
 
 		void spawnObstacle(sf::Vector2i position, float cell_width, float cell_height);
+		void destroy();
 
 	public:
 		ElementService();
@@ -22,7 +24,10 @@ namespace Element
 		void update();
 		void render();
 
+		void reset();
 		const void spawnElements(std::vector<ElementData>& element_data_list, float cell_width, float cell_height);
 		std::vector<sf::Vector2i> getElementsPositionList();
+
+		bool processElementsCollision(LinkedList::Node* head_node);
 	};
 }
